391877e37e763c158c096b7cc0cd73893fd9f02d
CaptainFlint1715 <chanseshirley123@gmail.com>
2023-06-18 13:10:41 -0500

    added CharacterStory and CharacterChoice models, relationships between, and seed json

---
+++ models/CharacterStory.js
@@ -0,0 +1,45 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class CharacterStory extends Model { }
+
+CharacterStory.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    description: {
+      type: DataTypes.STRING,
+    },
+    user_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'user',
+        key: 'id',
+      },
+    },
+    character_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'character',
+        key: 'id',
+      },
+    },
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'characterStory',
+  }
+);
+
+module.exports = CharacterStory;
--- models/index.js
+++ models/index.js
@@ -1,14 +1,16 @@
 const User = require('./User');
 const Character = require('./Character');
+const CharacterChoice = require('./CharacterChoice');
+const CharacterStory = require('./CharacterStory')
 const Scene = require('./Scene')
 const Choice = require('./Choice')
 
-User.hasMany(Character, {
+User.hasMany(CharacterStory, {
   foreignKey: 'user_id',
   onDelete: 'CASCADE'
 });
 
-Character.belongsTo(User, {
+CharacterStory.belongsTo(User, {
   foreignKey: 'user_id'
 });
 
@@ -31,4 +33,24 @@ Scene.hasMany(Choice, {
   as: 'triggeringChoices'
 });
 
-module.exports = { User, Character, Scene, Choice };
+// CharacterStory model
+CharacterStory.hasMany(CharacterChoice, {
+  foreignKey: 'character_story_id',
+});
+
+// CharacterChoice model
+CharacterChoice.belongsTo(CharacterStory, {
+  foreignKey: 'character_story_id',
+});
+
+CharacterChoice.belongsTo(Choice, {
+  foreignKey: 'choice_id',
+});
+
+// Choice model
+Choice.hasMany(CharacterChoice, {
+  foreignKey: 'choice_id',
+});
+
+
+module.exports = { User, Character, CharacterStory, CharacterChoice, Scene, Choice };
