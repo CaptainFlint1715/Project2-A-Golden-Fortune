9d487f7c9b74fcce7d6a53c96a11964ed07640ab
Penguuu <pengvang95@yahoo.com>
2023-06-19 19:22:12 -0500

    adjusting scenes to gameplay

--- public/js/gameplay.js
+++ public/js/gameplay.js
@@ -1,22 +1,21 @@
-function chooseOption(option) {
-  // Perform any necessary logic based on the chosen option
-  switch (option) {
-    case 1:
-      // Perform actions for Option 1
-      console.log('Option 1 chosen');
-      break;
-    case 2:
-      // Perform actions for Option 2
-      console.log('Option 2 chosen');
-      break;
-    case 3:
-      // Perform actions for Option 3
-      console.log('Option 3 chosen');
-      break;
-    default:
-      console.log('Invalid option chosen');
-  }
+fetch('/path/to/Scenedata.json')
+      .then(response => response.json())
+      .then(data => {
+        // Update the scene text
+        const sceneTextElement = document.getElementById('scene-text');
+        sceneTextElement.textContent = data[0].text; // Display the first scene text initially
 
-  // Redirect or update the scene as per the chosen option
-  // Add your code to navigate to the next scene or update the content dynamically
-}
+        // Add event listeners for the options
+        document.getElementById('option1').addEventListener('click', () => chooseOption(data[0].id));
+        document.getElementById('option2').addEventListener('click', () => chooseOption(data[1].id));
+        document.getElementById('option3').addEventListener('click', () => chooseOption(data[2].id));
+      })
+      .catch(error => {
+        console.error('Error loading scene data:', error);
+      });
+
+    // Function to handle option selection
+    function chooseOption(optionId) {
+      // Handle the chosen option here
+      console.log('Option chosen:', optionId);
+    }
